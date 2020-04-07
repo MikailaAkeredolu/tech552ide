625058a69815f4178fccc66f8b373ebaabb5b5cd
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2020-04-07 09:08:22 -0400

    uploaded files

---
+++ app.js
@@ -0,0 +1,36 @@
+function compile() {
+    var html = document.getElementById("html");
+    var css = document.getElementById("css");
+    var js = document.getElementById("js");
+    var code = document.getElementById("code").contentWindow.document;
+  
+    document.body.onkeyup = function() {
+      code.open();
+      code.writeln(
+        html.value +
+          "<style>" +
+          css.value +
+          "</style>" +
+          "<script>" +
+          js.value +
+          "</script>"
+      );
+      code.close();
+    };
+  }
+  
+  compile();
+
+  /*
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<p>I am a paragraph</p>
+</body>
+</html>
+
+  */
